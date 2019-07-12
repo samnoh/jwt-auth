f22b80bd8cba22b27e674ccdaeca29b87aed0e36
Sam Noh <samnoh21@outlook.com>
2019-07-12 15:58:15 +1200

    mod - mongoSanitizer

--- utils/sanitizer.js
+++ utils/sanitizer.js
@@ -1,5 +1,16 @@
-const mongoSanitizer = text => {
-    return text.replace(/[\/<>${}:]/g, '');
+const mongoSanitizer = body => {
+    if (typeof body !== 'object') {
+        return;
+    }
+
+    const newBody = { ...body };
+
+    for (key in newBody) {
+        let value = newBody[key];
+        newBody[key] = value.replace(/[\/<>${}:]/g, '');
+    }
+
+    return newBody;
 };
 
 module.exports = mongoSanitizer;
