1ea8ca3af8e39d9d79985522c99aa8be879438da
Sam Noh <samnoh21@outlook.com>
2019-07-14 23:53:55 +1200

    mod - form.js

--- public/js/form.js
+++ public/js/form.js
@@ -1,9 +1,8 @@
 $('form').submit(function(e) {
-    e.preventDefault();
     var inputs = $('form :input');
     var values = {};
     inputs.each(function() {
         values[this.name] = $(this).val();
     });
-    $('form').submit();
+    return;
 });
