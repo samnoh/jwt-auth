5fc00c312d7b413c7e836006e4dfca84b3099654
Sam Noh <samnoh21@outlook.com>
2019-07-15 12:27:26 +1200

    add - error messages

--- public/js/form.js
+++ public/js/form.js
@@ -1,13 +1,27 @@
 $('form').submit(function(e) {
-    var inputs = $('form :input');
-    var values = {};
+    let inputs = $('form :input');
+    let sanitizerRegex = RegExp(/[${}<>&'"]/);
+
     inputs.each(function() {
-        values[this.name] = $(this).val();
+        const value = $(this).val();
+
+        if (this.name === '_csrf') {
+            return true; // skip
+        }
+
+        if (sanitizerRegex.test(value)) {
+            e.preventDefault();
+            $('#alert-message').text('Inputs cannot contain <, >, $, &, {, }, \', " or /.');
+            $('#alert-box').show();
+            return false;
+        }
+
+        if (this.name === 'password' && value.length < 3) {
+            e.preventDefault();
+            $('#alert-message').text('Password length should be more than or equal to 3.');
+            $('#alert-box').show();
+        }
     });
 
-    if (true) {
-        return;
-    }
-
-    e.preventDefault();
+    return;
 });
